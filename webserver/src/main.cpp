18189f57d49554e84cce0c2e3331bfc6ed0f94ce
bernardo <bde-albu@42nice.com>
2025-08-04 15:10:57 +0200

    webserver-batman

---
+++ webserver/src/main.cpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bde-albu <bde-albu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/04 10:26:13 by bde-albu          #+#    #+#             */
+/*   Updated: 2025/08/04 13:52:34 by bde-albu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <arpa/inet.h>
+#include <cstring>
+#include <iostream>
+#include <unistd.h>
+
+int main(void)
+{
+	// BERNARDO
+	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	sockaddr_in server_addr;
+
+	std::memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	server_addr.sin_port = htons(8080);
+
+	bind(server_fd, (sockaddr*)&server_addr, sizeof(server_addr));
+
+	listen(server_fd, 1);
+
+	std::cout << "Server is listening on port 8080" << std::endl;
+
+	sockaddr_in client_addr;
+	socklen_t client_len = sizeof(client_addr);
+	int client_fd = accept(server_fd, (sockaddr*)&client_addr, &client_len);
+
+	std::cout << "Connected:" << inet_ntoa(client_addr.sin_addr) << std::endl;
+
+	// LIEN
+	char buffer[1024];
+	std::memset(buffer, 0, sizeof(buffer));
+	recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+
+
+	const char* reply = "Hello Word!\n";
+	send(client_fd, reply, strlen(reply), 0);
+
+	close(client_fd);
+	close(server_fd);
+
+	return 0;
+}
