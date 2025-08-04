34a8eda2956edf45faab2311e0c080375069f89d
bernardo <bde-albu@42nice.com>
2025-08-04 15:35:42 +0200

    poll

--- webserver/src/main.cpp
+++ webserver/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bde-albu <bde-albu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 10:26:13 by bde-albu          #+#    #+#             */
-/*   Updated: 2025/08/04 13:52:34 by bde-albu         ###   ########.fr       */
+/*   Updated: 2025/08/04 15:28:00 by bde-albu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,15 @@
 #include <cstring>
 #include <iostream>
 #include <unistd.h>
+#include <poll.h>
+#include <vector>
 
 int main(void)
 {
+	std::vector <pollfd> fds;
+	pollfd listener_fd;
+
+
 	// BERNARDO
 	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -32,6 +38,14 @@ int main(void)
 	listen(server_fd, 1);
 
 	std::cout << "Server is listening on port 8080" << std::endl;
+
+	listener_fd.fd = server_fd;
+	listener_fd.events = POLLIN;
+	fds.push_back(listener_fd);
+
+	while (1) {
+		
+	}
 
 	sockaddr_in client_addr;
 	socklen_t client_len = sizeof(client_addr);
